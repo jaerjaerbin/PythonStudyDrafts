ad35a6ec08e6b95f06a1f4a49d5ff9fca219cf63
alucardthefish <sergiop18@gmail.com>
2023-04-19 21:22:27 -0500

    Use method print params end and sep

---
+++ print_end_func_param.py
@@ -0,0 +1,10 @@
+
+numbers = range(0, 11, 2)
+
+print("Numbers being printed in line: ")
+for num in numbers:
+    print(num, end=" ")
+
+print("")
+print("Printing several strings separated by comma:")
+print("red", "green", "blue", sep=", ")
