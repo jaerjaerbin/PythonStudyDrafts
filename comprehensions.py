bfbc65d7886072549ec039cd16eb01c29293608e
alucardthefish <sergiop18@gmail.com>
2024-03-10 01:17:32 -0500

    Add dictionary comprehension examples

--- comprehensions.py
+++ comprehensions.py
@@ -13,3 +13,18 @@ team_2: list[str] = ["Evan", "Jake", "Randy"]
 new_teams: list[tuple] = [(x, y) for x in team_1 for y in team_2]
 
 print(f"{new_teams = }")
+
+# square_dict = dict()
+#
+# for num in numbers:
+#     square_dict[num] = num**2
+
+square_dict: dict = {num: num**2 for num in numbers}
+
+print(f"{square_dict = }")
+
+old_price: dict = {"milk": 1.02, "coffe": 2.5, "bread": 2.5}
+
+new_price: dict = {key: value*1.5 if value > 2 else value for (key, value) in old_price.items()}
+
+print(f"{new_price = }")
